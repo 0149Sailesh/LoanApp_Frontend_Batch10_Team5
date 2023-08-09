929c5739ab6c8278ebc4b79f32d677f0b1b77c1d
Rajakumar S <sgokulraj1525@gmail.com>
2023-08-09 17:26:57 +0530

    dashboard

---
+++ loan-app/src/containers/DashBoardContainer.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+//import { Homepage } from '../components/HomePage';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Navbar from 'react-bootstrap/Navbar';
+export function DashBoardContainer() {
+    return (
+        <div style={{
+            height:'100vh',
+            backgroundImage: "url(" + "/icons/new-wf.png" + ")",
+            backgroundPosition: 'top',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+        }}>
+            <Navbar expand="lg" className="bg-body-tertiary d-flex justify-content-center">
+                <Container className='"d-flex justify-content-center'>
+                    <Navbar.Brand href="#home">Dash Board</Navbar.Brand>
+                </Container>
+            </Navbar>
+            <div  className='mt-5 d-flex justify-content-center'>
+                <Card style={{ width: '18rem' }} className='p-4 m-4'>
+                    <Card.Img variant="top" src="icons/loan-ig.png" height={140} className='px-5' />
+                    <Card.Body className='d-flex justify-content-center flex-column'>
+                        <Card.Title>View Loans</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button className='mt-2 bg-danger text-warning border-0 fw-bold' variant="primary">View</Button>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }} className='p-4 m-4'>
+                    <Card.Img variant="top" src="icons/form-g.png" height={140} />
+                    <Card.Body className='d-flex justify-content-center flex-column'>
+                        <Card.Title>Apply For Loans</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button className='mt-2 bg-danger text-warning border-0 fw-bold' variant="primary">Apply</Button>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }} className='p-4 m-4'>
+                    <Card.Img variant="top" src="icons/cart.png" height={140} className='px-5' />
+                    <Card.Body className='d-flex justify-content-center flex-column'>
+                        <Card.Title>View Items Purchased</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button className='mt-2 bg-danger text-warning border-0 fw-bold' variant="primary">View</Button>
+                    </Card.Body>
+                </Card>
+
+
+            </div>
+        </div >
+    );
+}
--- loan-app/src/routes/routes.jsx
+++ loan-app/src/routes/routes.jsx
@@ -1,10 +1,15 @@
 import { HomePageContainer } from '../containers/HomePageContainer';
-
+import { DashBoardContainer } from '../containers/DashBoardContainer';
 export const publicRoutes = [
   {
     url: '/',
     component: HomePageContainer,
     name: 'HomePageContainer'
+  },
+  {
+    url: '/dash-board',
+    component: DashBoardContainer,
+    name: 'DashBoardContainer'
   }
 ];
 
