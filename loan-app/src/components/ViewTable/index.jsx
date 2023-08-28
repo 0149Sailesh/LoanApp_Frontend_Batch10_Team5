c1e13bb199443e03ee1d7c85f4dfbb892cc281cb
Rajakumar S <sgokulraj1525@gmail.com>
2023-08-28 22:20:19 +0530

    fix: margin in view in res

--- loan-app/src/components/ViewTable/index.jsx
+++ loan-app/src/components/ViewTable/index.jsx
@@ -45,8 +45,8 @@ export function ViewTable({keys, values,deleteHandler,modelHandler, showButton=t
                             {value.map((i, x)=> <div  className={styles.resRow}  ><span className='w-30'>{keys[x]}:</span> <span className=''>{i}</span></div>)}
                             {
                                 showButton ?  <td className={styles.actions}>
-                                <button className={`bg-info text-white  ${styles.btnActions}`} onClick={()=>modelHandler(value)} >{'Edit'}</button>
-                                <button className={` bg-danger text-white ${styles.btnActions}`} onClick={()=> deleteHandler(value[0])} >{'Delete'}</button>
+                                <button className={`bg-info text-white mt-2  ${styles.btnActions}`} onClick={()=>modelHandler(value)} >{'Edit'}</button>
+                                <button className={` bg-danger text-white mt-2 ${styles.btnActions}`} onClick={()=> deleteHandler(value[0])} >{'Delete'}</button>
                             </td>:''
                             }
                            
